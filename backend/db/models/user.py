dd3047476898d0a0a80d02a7d53a89e1a61a0a23
Elijah <elijahahmad03@gmail.com>
2025-06-08 15:19:39 +0100

    Data: Modelled postgres.

--- backend/db/models/user.py
+++ backend/db/models/user.py
@@ -1,9 +1,10 @@
-from typing import Optional
-from sqlmodel import Field
+from typing import Optional, List
+from sqlmodel import Field, Relationship
 from .base import TimestampModel
-
+from .repos import Repo
 class User(TimestampModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str = Field(unique=True, index=True)
     name: str
     firebase_uid: str = Field(unique=True, index=True) 
+    repos: List["Repo"] = Relationship(back_populates="user")
